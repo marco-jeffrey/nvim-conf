c1251221d87918940fc375367dc13fe0848ed7fb
MJPansa <marco.jeffrey.pansa@googlemail.com>
2023-11-21 17:12:30 +0100

    Initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+        "bash",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "sql",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
